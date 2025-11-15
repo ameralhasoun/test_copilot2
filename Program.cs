8bf47f1c30b4de6ee072ef3cbe67209d29558bbb
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-11-15 11:32:36 +0100

    add basic calculator

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Example
+{
+    public class Calculator
+    {
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            int result1 = calc.AddNumbers(5, 3);
+
+            Console.WriteLine($"AddNumbers result: {result1}");
+        }
+    }
+}
